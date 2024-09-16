3ae6b8ada14a91f6dda2e90270f3c7f9882ee257
kamlesh <k24suthar@gmail.com>
2024-09-16 11:43:56 +0530

    fix

--- app.js
+++ app.js
@@ -32,8 +32,7 @@ app.use('/test', testRoutes)
 app.use('/', eventRoutes);
 
 
-app.listen(4000, () => {
+
+app.listen(port, () => {
     console.log(`Server is running on ${port}`)
 })
-
-export default app;
